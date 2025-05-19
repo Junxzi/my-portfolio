913054f4d62802bf78bdcd6b93059fe02faf929a
Jun Ishikawa <junish826@gmail.com>
2025-05-19 17:10:40 +0900

    refactor

--- src/app/en/about/page.tsx
+++ src/app/en/about/page.tsx
@@ -3,7 +3,7 @@ export default function About() {
     <section className="space-y-4 max-w-2xl mx-auto">
       <h2 className="text-3xl font-bold">About Me</h2>
       <p>
-        I'm Jun!
+        I&apos;m Jun!
       </p>
       <p>
         I like playing games.
